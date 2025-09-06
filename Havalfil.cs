b5b89c292f0eba34c47234a282cdeaede9f64236
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-06 18:03:31 +0200

    Add project files.

---
+++ Havalfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mondaytoday
+{
+    internal class Havalfil
+    {
+        // Haval is student
+        // This is Haval's file
+    }
+}
